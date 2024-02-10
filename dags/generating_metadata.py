2953de14326b34498e83fc652e163259af51227f
Juan_Menezes <juan.menezes@outlook.com>
2024-02-10 17:37:37 -0300

    :cop: updates acerca de metricas e dataviz

--- dags/generating_metadata.py
+++ dags/generating_metadata.py
@@ -7,7 +7,7 @@ import pandas as pd
 from collections import Counter
 
 def gerar_metadados(**op_kwargs):
-    df = pd.read_csv(op_kwargs['input_path'], delimiter=';')
+    df = pd.read_csv(op_kwargs['input_path'], delimiter=',')
     # ! quando for para rodar os dados da ufrn o delimiter que ser modificado para ; e quando for ufrpe para ,
     # Coletar metadados
     tipagem_das_colunas = df.dtypes
@@ -50,31 +50,36 @@ def avaliacoes_criterios(**op_kwargs):
         # Cálculo da confiabilidade, precisao, completude, acessibilidade, consistencia
         n_colunas = json_metadado['n_colunas']
         quantidade_object = json_metadado['contador_tipagem'].get("object",0)
-        confiabilidade = (n_colunas - quantidade_object) * 100 / n_colunas
+        confiabilidade = (n_colunas - quantidade_object) / n_colunas
         
         # Cálculo da completude
         n_colunas = len(json_metadado["valores_nulos_por_coluna"])
         n_valores_nulos_coluna = sum(value == 0 for value in json_metadado["valores_nulos_por_coluna"].values())
-        completude = (1 - (n_valores_nulos_coluna / n_colunas)) * 100
+        completude = (1 - (n_valores_nulos_coluna / n_colunas))
 
         # Cálculo da consistência
         valores_distintos_por_coluna = json_metadado["valores_distintos_por_coluna"]
         inconsistencias = sum(value > 10 for value in valores_distintos_por_coluna.values())
         # Aqui, o 1 é subtraído para enfatizar a consistência. Se não houver inconsistências (inconsistencias=0inconsistencias=0), a fórmula retornará 100100, indicando consistência completa. Se todas as colunas forem inconsistentes, a fórmula retornará 00.
-        consistencia = (1 - (inconsistencias / n_colunas)) * 100
+        consistencia = (1 - (inconsistencias / n_colunas))
 
         # Cálculo da precisão
-        precisao = (inconsistencias / n_colunas) * 100
+        precisao = (inconsistencias / n_colunas)
         # Aqui, não há subtração de 11, pois a precisão é medida diretamente pela proporção de valores distintos em relação ao total de colunas. Essa métrica indica a "precisão" dos dados em termos de diferentes valores presentes.
 
-        # Exibição das métricas
-        print(f"Credibilidade da tabela {file_path} foi de : {confiabilidade}%")
-        print(f"Completude da tabela {file_path} foi de : {completude}%")
-        print(f"Consistência da tabela {file_path} foi de : {consistencia}%\n")
-        print(f"Precisão da tabela {file_path} foi de : {precisao}%")
-
+        # TODO deixar mais modular de forma que facilite a analise, pq hoje tenho agregar os jsons manualmente
+        analysis_tables = {
+            "nome_da_tabela": file_path,
+            "credibilidade": confiabilidade,
+            "completude": completude,
+            "consistencia": consistencia,
+            "precisao": precisao
+        }
         # lembrando que os dados gerados vou montar como json para plotar isso em outro código
-
+        # Salvar as informações em um arquivo JSON
+        nome_do_arquivo = os.path.basename(file_path)
+        with open(f'data/analysis/{nome_do_arquivo}', 'w') as json_file:
+            json.dump(analysis_tables, json_file, indent=4)
 
 default_args = {
     'owner': 'airflow',
@@ -87,17 +92,17 @@ dag = DAG(
     default_args=default_args,
     description='Uma DAG para analisar metadados de um arquivo CSV',
     schedule_interval=None,  # Define a frequência de execução da DAG (None para executar manualmente)
-    tags=["metaqd", "tcc-ufrn"]
+    tags=["metaqd", "tcc-ufrpe"]
 )
 
-parent_dir_source = 'data/source/ufrn/'
+parent_dir_source = 'data/source/ufrpe/'
 
 # Para cada diretorio com dados eu gero os seus respectivos metadados
 folders_names = [folder for folder in os.listdir(parent_dir_source) if os.path.isdir(os.path.join(parent_dir_source, folder))]
 
 for folder_name in folders_names:
-    input_path = f'data/source/ufrn/{folder_name}/{folder_name}.csv'
-    output_path = f'data/metadata/ufrn/{folder_name}_metadados.json' 
+    input_path = f'data/source/ufrpe/{folder_name}/{folder_name}.csv'
+    output_path = f'data/metadata/ufrpe/{folder_name}_metadados.json' 
     table_name = folder_name
 
     gerar_metadados_task = PythonOperator(
@@ -107,7 +112,7 @@ for folder_name in folders_names:
         dag=dag,
     )
 
-parent_dir_metadata = 'data/metadata/ufrn/'
+parent_dir_metadata = 'data/metadata/ufrpe/'
 
 metadados_files_path = [os.path.join(parent_dir_metadata, file) for file in os.listdir(parent_dir_metadata) if os.path.isfile(os.path.join(parent_dir_metadata, file))]
 
