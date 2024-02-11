b2529adcd7a3a6a9e4a84ce48d41a256692cb7e4
Juan_Menezes <juan.menezes@outlook.com>
2024-02-11 17:44:29 -0300

    :muscle: finalizacao da dag e da analise

--- dags/generating_metadata.py
+++ dags/generating_metadata.py
@@ -8,8 +8,8 @@ import pandas as pd
 from collections import Counter
 
 def gerar_metadados(**op_kwargs):
-    df = pd.read_csv(op_kwargs['input_path'], delimiter=',')
-    # ! quando for para rodar os dados da ufrn o delimiter que ser modificado para ; e quando for ufrpe para ,
+    df = pd.read_csv(op_kwargs['input_path'], delimiter=';')
+    # ! quando for para rodar os dados da ufrn o delimiter que ser modificado para ; e quando for ufrn para ,
     
     # Coletar metadados
     tipagem_das_colunas = df.dtypes
@@ -72,7 +72,7 @@ def avaliacoes_criterios(**op_kwargs):
 
         # ! Criando o processo de salvar essas métricas em um CSV para plot posterior
         # Definir o nome do arquivo CSV
-        nome_arquivo_csv = 'data/analysis/metricas_ufrpe.csv'
+        nome_arquivo_csv = 'data/analysis/metricas_ufrn.csv'
 
         # Verificar se o arquivo CSV já existe
         arquivo_existente = False
@@ -98,7 +98,7 @@ def avaliacoes_criterios(**op_kwargs):
             parte_especifica, nome_arquivo_sem_extensao = os.path.split(file_path)
             nome_da_tabela = str(nome_arquivo_sem_extensao.split(".")[0])
 
-            writer.writerow([f"ufrpe_{nome_da_tabela}", confiabilidade, completude, consistencia, precisao])
+            writer.writerow([f"ufrn_{nome_da_tabela}", confiabilidade, completude, consistencia, precisao])
             print(f'Dados adicionados ao arquivo CSV "{nome_arquivo_csv}" com sucesso.')
 
             # lembrando que os dados gerados vou montar como json para plotar isso em outro código
@@ -118,14 +118,14 @@ dag = DAG(
     tags=["metaqd", "tcc"]
 )
 
-parent_dir_source = 'data/source/ufrpe/'
+parent_dir_source = 'data/source/ufrn/'
 
 # Para cada diretorio com dados eu gero os seus respectivos metadados
 folders_names = [folder for folder in os.listdir(parent_dir_source) if os.path.isdir(os.path.join(parent_dir_source, folder))]
 
 for folder_name in folders_names:
-    input_path = f'data/source/ufrpe/{folder_name}/{folder_name}.csv'
-    output_path = f'data/metadata/ufrpe/{folder_name}_metadados.json' 
+    input_path = f'data/source/ufrn/{folder_name}/{folder_name}.csv'
+    output_path = f'data/metadata/ufrn/{folder_name}_metadados.json' 
     table_name = folder_name
 
     gerar_metadados_task = PythonOperator(
@@ -135,7 +135,7 @@ for folder_name in folders_names:
         dag=dag,
     )
 
-parent_dir_metadata = 'data/metadata/ufrpe/'
+parent_dir_metadata = 'data/metadata/ufrn/'
 
 metadados_files_path = [os.path.join(parent_dir_metadata, file) for file in os.listdir(parent_dir_metadata) if os.path.isfile(os.path.join(parent_dir_metadata, file))]
 
